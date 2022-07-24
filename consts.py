956c52ccb4afd64d86da3ca0cd6a874a5a65ee9f
cumberland92 <cordell.hammon@gmail.com>
2022-07-24 09:59:00 -0500

    made it work by switching to uppercase lol

--- consts.py
+++ consts.py
@@ -1,9 +1,9 @@
 def removeVowels(s):
-      s = s.replace("a","")
-      s = s.replace("e","")
-      s = s.replace("i","")
-      s = s.replace("o","")
-      s = s.replace("u","")
+      s = s.replace("A","")
+      s = s.replace("E","")
+      s = s.replace("I","")
+      s = s.replace("O","")
+      s = s.replace("U","")
       return s
 
 def loadDictionary():
@@ -15,31 +15,28 @@ def loadDictionary():
     for i in range(len(dicList)):
         dicList[i] = dicList[i].replace("\n", "")
     for word in dicList:
-        print("Before removing, word={}".format(word))
         word=removeVowels(word)
-        print("after removing, word={}".format(word))
-    #     length = len(word)
-    #     if(length==0):
-    #         continue
-    #     if(length in englishWords.keys()):
-    #         englishWords[length].append(word)
-    #     else:
-    #         englishWords.update({length: [word]})
-    # dictionaryFile.close()
+        length = len(word)
+        if(length==0):
+            continue
+        if(length in englishWords.keys()):
+            englishWords[length].append(word)
+        else:
+            englishWords.update({length: [word]})
+    dictionaryFile.close()
     return englishWords
 
 
 
 ENGLISH_WORDS = loadDictionary()
 print(ENGLISH_WORDS)
-# for i in sorted(ENGLISH_WORDS.keys()):
-#     seen = set()
-#     containsDuplicates = False
-#     for j in range(len(ENGLISH_WORDS[i])):
-#         if ENGLISH_WORDS[i][j] in seen:
-#             containsDuplicates = True
-#             print("{} does not work because of {}".format(i, ENGLISH_WORDS[i][j]))
-#             break
-#         if(j==len(ENGLISH_WORDS[i])-1):
-#             print("The smallest number of consonants is {}".format(i))
-#             quit()
+for i in sorted(ENGLISH_WORDS.keys()):
+    seen = set()
+    for j in range(len(ENGLISH_WORDS[i])):
+        if ENGLISH_WORDS[i][j] in seen:
+            print("{} does not work because of {}".format(i, ENGLISH_WORDS[i][j]))
+            break
+        if(j==len(ENGLISH_WORDS[i])-1):
+            print("The smallest number of consonants is {}".format(i))
+            quit()
+        seen.add(ENGLISH_WORDS[i][j])
